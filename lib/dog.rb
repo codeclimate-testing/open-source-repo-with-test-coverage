45484d86d771cfca63240f4c4b2ecaeb648a543a
Max Jacobson <max@hardscrabble.net>
2016-07-15 11:38:03 -0400

    regular push

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,6 +49,15 @@
 # TODO: improve this class
 # TODO: improve this class
 # TODO: improve this class
+# TODO: improve this class
+# TODO: improve this class
+# TODO: improve this class
+# TODO: improve this class
+# TODO: improve this class
+# TODO: improve this class
+# TODO: improve this class
+# TODO: improve this class
+# TODO: improve this class
 class Dog
   def initialize(name)
     @name = name
