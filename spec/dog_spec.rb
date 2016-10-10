8282865669c156bb1c1ae7ba12c34390c7c0f36b
Max Jacobson <max@hardscrabble.net>
2016-10-10 19:08:02 -0400

    increase coverage!

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe Dog do
     it "is super excited about its name" do
       expect(Dog.new("Milo").name).to eq("Milo!!!")
     end
+
+    it "is super excited about its age" do
+      expect(Dog.new("Milo").age_in_dog_years).to eq(405)
+    end
   end
 end
