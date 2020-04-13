12fe500ca9eaf3579202d5a384b4f22395e3ab0e
Federico Moya <federicomoyamartin@gmail.com>
2020-04-13 13:00:34 -0300

    Cause some troubles

---
+++ lib/spider.rb
@@ -0,0 +1,25 @@
+class Spider
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    "#{@name}!!!"
+  end
+
+
+
+  def speak
+    "psssssss"
+  end
+
+
+
+  def untested
+    puts "foo"
+  end
+
+  def also_untested
+    puts "foo"
+  end
+end
